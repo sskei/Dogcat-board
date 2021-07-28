ef5d1f21af8acab4ed10f723673509594510ff96
sskei <87478406+sskei@users.noreply.github.com>
2021-07-28 17:59:24 +0900

    Add files via upload
    test3

---
+++ DogCat/main.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons =  document.querySelector('.navbar_icons');
+
+// api 사용 
+toggleBtn.addEventListener('click',()=>{
+
+    menu.classList.toggle('active'); 
+    // 마우스클릭햇을때 토글링
+    icons.classList.toggle('active');
+});
