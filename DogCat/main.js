3b5e3d3319ece0e92233e1fa5b9bdaa2907a38ac
sskei <87478406+sskei@users.noreply.github.com>
2021-07-29 23:44:17 +0900

    Add files via upload

--- DogCat/main.js
+++ DogCat/main.js
@@ -9,3 +9,11 @@ toggleBtn.addEventListener('click',()=>{
     // 마우스클릭햇을때 토글링
     icons.classList.toggle('active');
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
